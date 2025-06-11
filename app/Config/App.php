661055e42f992f8e881226c8cc7831f6d6bf500e
SrJordy <jvilcacundoc@uteq.edu.ec>
2025-06-11 16:49:53 -0500

    update url

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      *    http://example.com/
      */
-    public string $baseURL = '34.204.30.64';
+    public string $baseURL = 'http://Casanueva.us-east-1.elasticbeanstalk.com/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
