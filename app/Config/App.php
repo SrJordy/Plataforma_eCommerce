caba05bb2570816b8879a3df70c8e602edce4b06
SrJordy <jvilcacundoc@uteq.edu.ec>
2025-06-11 16:46:54 -0500

    update url

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      *    http://example.com/
      */
-    public string $baseURL = '>34.204.30.64';
+    public string $baseURL = '34.204.30.64';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
